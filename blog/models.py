811bf2a73cedd74a394a93472382eb78d2c42b8b
Madiou DIALLO <mahdiou.diallo@outlook.fr>
2024-10-20 16:08:51 -0400

    Modification du fichier admin.py

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category)
     approved = models.BooleanField(default=False)
 
     def __str__(self):
