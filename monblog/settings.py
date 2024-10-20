f0a9d781a13b88e78c3a4ff516fa2d63c44a396c
Madiou DIALLO <mahdiou.diallo@outlook.fr>
2024-10-20 16:06:50 -0400

    Modification du fichier admin.py

--- monblog/settings.py
+++ monblog/settings.py
@@ -47,7 +47,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.example.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mahdiou.diallo@gmail.com'
+EMAIL_HOST_USER = 'mahdiou.diallo@localhost'
 EMAIL_HOST_PASSWORD = 'Mouminia25'
 
 MIDDLEWARE = [
