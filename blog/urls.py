a3c3332a54f678f6fb88832664728b969c761583
Madiou DIALLO <mahdiou.diallo@outlook.fr>
2024-10-20 16:11:38 -0400

    Modification du fichier admin.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('post/add/', views.add_post, name='add_post'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/comment/', views.add_comment, name='add_comment'),
+    path('post/<int:pk>/category/', views.add_comment, name='category'),
 ]
