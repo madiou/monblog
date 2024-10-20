1fe77a6bb70646a6f69a31594020e7e33ba1d759
Madiou DIALLO <mahdiou.diallo@outlook.fr>
2024-10-20 15:42:14 -0400

    Modification du fichier admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
+# blog/admin.py
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date_posted', 'approved')
+    list_filter = ('approved', 'date_posted', 'author')
+    search_fields = ('title', 'content')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'email', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on', 'post')
+    search_fields = ('name', 'email', 'body')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+
