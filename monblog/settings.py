751d1290b2c05b8d60199e1f858126ec87a3600e
Madiou DIALLO <mahdiou.diallo@outlook.fr>
2024-10-20 19:29:08 -0400

    Import de os dans settings.py

--- monblog/settings.py
+++ monblog/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount',  
 ]
 
 AUTHENTICATION_BACKENDS = (
@@ -61,6 +62,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
 ]
 
 ROOT_URLCONF = "monblog.urls"
